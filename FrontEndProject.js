5c6c9464ff1d73fd0641e3ae8102407a20bac151
Taylor Hanson <tnhanson11@gmail.com>
2022-03-03 08:24:17 -0600

    Flight form updated, weather widget and restaurant code added but not complete.

--- FrontEndProject.js
+++ FrontEndProject.js
@@ -1,62 +1,47 @@
-FrontEndProject.js;
-
-document.addEventListener("DOMcontentLoaded", function () {
+//FrontEndProject.js;
+let restaurantArray = [{'picture': 'test.png', 'title': 'Chilis', 'reviews': ['review1', 'review2', 'review3']}, {'picture': 'test2.png', 'title': 'Applebees', 'reviews': ['review1', 'review2', 'review3']}]
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM is loaded")
     function renderRestaurant(restaurantArray) {
         let restaurantHTML = restaurantArray.map((currentRestaurant) => {
             return `
-        <div class=cardContainer>   
-            <div class="card" style="width: 18rem;">
-            <img src="${current.Restaurant.picture}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${currentRestaurant.Title}</h5>
-              <p class="card-text">${currentRestaurant.Reviews}</p>
-              <a href="#" class="btn btn-primary">Website</a>
-            </div>
-        </div>`
+          
+                <div class="card" style="width: 18rem;">
+                <img src="${currentRestaurant.picture}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${currentRestaurant.title}</h5>
+                <p class="card-text">${currentRestaurant.reviews}</p>
+                <a href="#" class="btn btn-primary">Website</a>
+                </div>
+            `;
 
         });
+        return restaurantHTML.join("");
     }
-
-    return restaurantHTML.join("");
+    let flightButton = document.getElementById('flight-button');
+    let cardContainer = document.getElementById('cardContainer'); 
+    flightButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log(event.target[1].value);
+        let search_string = event.target[1].value;
+    
+       
+        fetch(`https://airlabs.co/api/v9/flights?api_key=8f6b8310-9485-404f-a71a-eab743737e9d`)
+            .then((response) => response.json())
+            .then((flight_to_data ) => {
+                console.log(flight_to_data );
+                cardContainer.innerHTML = renderRestaurant(restaurantArray);  
+                // fetch('https://api.yelp.com/v3/businesses/GGs_dGvtT4SwQzpjI36UOpO81iXY6ODulTXvnKkJUlU0QFGU2-SeF7TrZgM4JuJH54aA8yrMTea90A-9dnsPZ6LtJLLTFY-0pM_Z-SF-WWCr29X3IKN3Kl21jTkRYnYx')
+                // .then((response) => response.json())
+                // .then((rest_data) => {
+                //     console.log(rest_data)
+    
+                //     title.innerHTML = `info here`
+                // })
+                // .catch(error => {
+                //     console.error(error);
+                // });
+            }) 
+    });  
+    
 })
-
-
-let cardContainer = document.getElementById('cardContainer');
-let flying_to = flight_data 
-let rest_data = rest_data
-    //results from flight search 
-
- 
-    data.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log(event.target.data.value);
-    let search_string = event.target.data.value;
-
-        document
-        .getElementById("data")
-        .addEventListener("submit", function (e) {
-            fetch(`https://api.yelp.com/v3GGs_dGvtT4SwQzpjI36UOpO81iXY6ODulTXvnKkJUlU0QFGU2-SeF7TrZgM4JuJH54aA8yrMTea90A-9dnsPZ6LtJLLTFY-0pM_Z-SF-WWCr29X3IKN3Kl21jTkRYnYx`)
-                .then((response) => response.json())
-                .then((flight_data) => {
-                    console.log(flight_data);
-
-
-                 fetch('https://api.yelp.com/v3GGs_dGvtT4SwQzpjI36UOpO81iXY6ODulTXvnKkJUlU0QFGU2-SeF7TrZgM4JuJH54aA8yrMTea90A-9dnsPZ6LtJLLTFY-0pM_Z-SF-WWCr29X3IKN3Kl21jTkRYnYx')
-                    .then((response) => response.json())
-                    .then((rest_data) => {
-                        console.log(rest_data)
-
-                        title.innerHTML = `info here`
-
-                        .catch(error => {
-                            console.error(error);
-                        });
-                    });
-                });
-            }); 
-        });   
-    });
-})    
-
-
-
